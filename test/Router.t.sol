9999d818361513dca5830e7dd8c7bcff62f89690
Szymon Szlachtowicz <38212223+Szymx95@users.noreply.github.com>
2023-03-24 12:55:53 +0100

    🗝️ Add incrementer (#2)

--- test/Router.t.sol
+++ test/Router.t.sol
@@ -4,21 +4,26 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "contracts/Router.sol";
 import "contracts/Extension.sol";
+import "contracts/Incrementer.sol";
 import "@dynamic-contracts/interface/IExtension.sol";
 
 contract RouterTest is Test {
     SimpleRouter public router;
-    Number public number;
 
 
     function setUp() public {
-        number = new Number();
+        Number number = new Number();
 
-        IExtension.Extension[] memory extensions = new IExtension.Extension[](1);
-        extensions[0].metadata = IExtension.ExtensionMetadata('number', 'number.storage', address(number));
+        IExtension.Extension[] memory extensions = new IExtension.Extension[](2);
+        extensions[0].metadata = IExtension.ExtensionMetadata('number', 'qwe.storage', address(number));
         extensions[0].functions = new IExtension.ExtensionFunction[](2);
         extensions[0].functions[0] = IExtension.ExtensionFunction(number.setNumber.selector, 'setNumber(uint256)');
         extensions[0].functions[1] = IExtension.ExtensionFunction(number.getNumber.selector, 'getNumber()');
+
+        Incrementer incrementer = new Incrementer();
+        extensions[1].metadata = IExtension.ExtensionMetadata('incrementer', 'incrementer.storage', address(incrementer));
+        extensions[1].functions = new IExtension.ExtensionFunction[](1);
+        extensions[1].functions[0] = IExtension.ExtensionFunction(incrementer.increment.selector, 'increment()');
 
         router = new SimpleRouter(extensions);
     }
@@ -26,5 +31,11 @@ contract RouterTest is Test {
     function testSetNumber(uint256 x) public {
         Number(address(router)).setNumber(x);
         assertEq(Number(address(router)).getNumber(), x);
+    }
+
+    function testIncrement() public {
+        Number(address(router)).setNumber(5);
+        Incrementer(address(router)).increment();
+        assertEq(Number(address(router)).getNumber(), 6);
     }
 }
