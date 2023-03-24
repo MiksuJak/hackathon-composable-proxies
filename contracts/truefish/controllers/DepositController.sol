f652b89a9eeb939b5e0ff37eab82bb2d2368fd52
Mikołaj Jakubowski <mikolaj.jakubowski@protonmail.com>
2023-03-24 14:25:13 +0100

    🏮 Fix beryllium (#7)

--- contracts/truefish/controllers/DepositController.sol
+++ contracts/truefish/controllers/DepositController.sol
@@ -2,28 +2,16 @@
 pragma solidity ^0.8.10;
 
 import {IDepositController} from "../interfaces/IDepositController.sol";
-import {IPortfolio} from "../interfaces/IPortfolio.sol";
 import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+import {IERC4626} from "../interfaces/IERC4626.sol";
 
 contract DepositController is IDepositController, Initializable {
-    ILenderVerifier public lenderVerifier;
-
-    function initialize(ILenderVerifier _lenderVerifier) external virtual initializer {
-        lenderVerifier = _lenderVerifier;
-    }
-
     function maxDeposit(address receiver) public view virtual returns (uint256) {
-        if (!lenderVerifier.isAllowed(receiver)) {
-            return 0;
-        }
-        return IPortfolio(msg.sender).maxSize() - IPortfolio(msg.sender).totalAssets();
+        return type(uint256).max;
     }
 
     function maxMint(address receiver) public view virtual returns (uint256) {
-        if (!lenderVerifier.isAllowed(receiver)) {
-            return 0;
-        }
         return previewDeposit(maxDeposit(receiver));
     }
 
@@ -32,9 +20,6 @@ contract DepositController is IDepositController, Initializable {
         uint256 assets,
         address receiver
     ) public view virtual returns (uint256, uint256) {
-        if (!lenderVerifier.isAllowed(receiver)) {
-            return (0, 0);
-        }
         return (previewDeposit(assets), 0);
     }
 
@@ -43,19 +28,16 @@ contract DepositController is IDepositController, Initializable {
         uint256 shares,
         address receiver
     ) public view virtual returns (uint256, uint256) {
-        if (!lenderVerifier.isAllowed(receiver)) {
-            return (0, 0);
-        }
         return (previewMint(shares), 0);
     }
 
     function previewDeposit(uint256 assets) public view returns (uint256 shares) {
-        return IPortfolio(msg.sender).convertToShares(assets);
+        return IERC4626(msg.sender).convertToShares(assets);
     }
 
     function previewMint(uint256 shares) public view returns (uint256) {
-        uint256 totalAssets = IPortfolio(msg.sender).totalAssets();
-        uint256 totalSupply = IPortfolio(msg.sender).totalSupply();
+        uint256 totalAssets = IERC4626(msg.sender).totalAssets();
+        uint256 totalSupply = IERC4626(msg.sender).totalSupply();
         if (totalSupply == 0) {
             return shares;
         } else {
